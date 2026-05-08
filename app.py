b13c87f0fd74ee89502d017ac2111c686286c822
tonydasen <tonydasen2023@126.com>
2026-05-08 23:33:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -450,4 +450,5 @@ def load_list():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000, threaded=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port, threaded=True)
